dafd781b1f5bddc0bdd7bb60a3d1b2e63dc69c62
BlockBaby <admin@Essketit.local>
2025-02-19 21:42:18 +0700

    Fix DisallowedHost issue 2

--- BDSProject/settings.py
+++ BDSProject/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,14 +77,7 @@ WSGI_APPLICATION = 'BDSProject.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME':  'postgres',
-        'USER':  'postgres',
-        'PASSWORD': '12345678',
-        'HOST':  'localhost',
-        'PORT':  '5432',
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 
